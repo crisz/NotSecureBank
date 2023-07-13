7c8d703f2b901798695245d2f0f3ebbe84e28fe4
e3ctrain <c.traina@reply.it>
2023-07-13 12:49:07 +0200

    Fix v11_SessionManagement

--- src/com/notsecurebank/api/TransferAPI.java
+++ src/com/notsecurebank/api/TransferAPI.java
@@ -7,12 +7,16 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
+import com.notsecurebank.model.Account;
+import com.notsecurebank.model.User;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.wink.json4j.JSONException;
 import org.apache.wink.json4j.JSONObject;
 
 import com.notsecurebank.util.OperationsUtil;
+
+import static com.notsecurebank.util.ServletUtil.getUser;
 
 @Path("transfer")
 public class TransferAPI extends NotSecureBankAPI {
@@ -41,6 +45,19 @@ public class TransferAPI extends NotSecureBankAPI {
             }
             creditActId = Long.parseLong(myJson.get("toAccount").toString());
             fromAccount = myJson.get("fromAccount").toString();
+            
+            User user = getUser(request);
+            boolean found = false;
+            for (Account account: user.getAccounts()) {
+                if (account.getAccountId() == Long.parseLong(fromAccount)) {
+                    found = true;
+                    break;
+                }
+            }
+            
+            if (!found) {
+                return Response.status(403).entity("{\"Error\": \"Invalid fromAccount parameter\"}").build();
+            }
             amount = Double.parseDouble(myJson.get("transferAmount").toString());
             message = OperationsUtil.doTransfer(request, creditActId, fromAccount, amount);
         } catch (JSONException e) {
